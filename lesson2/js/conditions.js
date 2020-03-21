16d32ddf8f5114dfa988d4aa8bef10240dcdaefa
Philip <heroesfill@gmail.com>
2020-03-21 10:56:07 +0300

    lesson2

---
+++ lesson2/js/conditions.js
@@ -0,0 +1,41 @@
+// Конструкции ветвления
+// используються,когда есть необходимость выполнить код в зависимости от условий
+let age=13;
+if (age >= 18) {
+    console.log("Добро пожаловать на сайат");
+} else {
+    console.log("Вы не можете посещать данный рессурс");
+}
+// if (условие) {
+//     блок кода выполняеться если условие true
+// }
+
+const GREEN="green";
+const BLUE="blue";
+const ORANGE="orange";
+const YELLOW="yellow";
+const WHITE="white";
+
+let season = 'spring';
+
+if (season === 'spring') {
+    document.body.style.background = YELLOW;
+} else if (season === 'summer') {
+    document.body.style.background = GREEN;
+} else if (season === 'autumn') {
+    document.body.style.background = ORANGE;
+} else if (season === 'winter') {
+    document.body.style.background = BLUE;
+}else {
+    document.body.style.background = WHITE;
+}
+
+let day = 'четверг';
+
+if (day === 'суббота'|| day === 'воскресенье') {
+    console.log("выходные дни");
+} else if (day === 'понедельник'||day === 'вторник'|| day ==='среда'|| day === 'четверг'||day === 'пятница') {
+    console.log("рабочая неделя");
+} else {
+    console.log("введите правильное значение");
+}
