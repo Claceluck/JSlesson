6cffe0ad3c50be2755b4ed04503de1e40eb1e2ce
Philip <heroesfill@gmail.com>
2020-04-18 10:15:32 +0300

    Начали проходить DOM

---
+++ lesson6/js/example.js
@@ -0,0 +1,26 @@
+let goods = {
+    piano: {
+        title: "Пианино",
+        price: 3000,
+        count: 25,
+        img: "https://picsum.photos/id/345/300"
+    },
+    guitar: {
+        title: "Гитара",
+        price: 1200,
+        count: 40,
+        img: "https://picsum.photos/id/988/300"
+    },
+    drum: {
+        title: "Барабаны",
+        price: 2700,
+        count: 12,
+        img: "https://picsum.photos/id/123/300"
+    },
+    flute: {
+        title: "Флейта",
+        price: 900,
+        count: 50,
+        img: "https://picsum.photos/id/162/300"
+    }
+};
