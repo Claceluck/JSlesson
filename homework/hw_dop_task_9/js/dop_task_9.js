b26b22f86d412e360698680faa5397224a61edf6
Philip <heroesfill@gmail.com>
2020-04-28 18:34:02 +0300

    Дом.работа 9

--- homework/hw_dop_task_9/js/dop_task_9.js
+++ homework/hw_dop_task_9/js/dop_task_9.js
@@ -6,13 +6,61 @@ let books = [
     {author: 'Гончаров', title: 'Обломов', count: 10}
 ];
 
+    
+for(let elem of books) {
 
-for (const elem of books) {
     let booksTable = document.getElementById('booksTable');
-    let title = createElemetn('h2');
-    title.innerText = `${elem.title} | ${elem.author} `;
 
-    booksTable.append(title);
+    let title = document.createElement('h3');
+    title.innerText = `Название произведения: ${elem.title}`;
+
+    let author = document.createElement('p');
+    author.innerText = `Автор: ${elem.author}`;
+
+    let form = document.createElement('form'); // создаю форму
+    form.setAttribute('action', '#');
+    form.setAttribute('method', 'post');
+    form.setAttribute('name','count');
+
+    let countForm = document.createElement('input');
+    countForm.setAttribute('type', 'number');
+    countForm.setAttribute('placeholder', 'Введите колличество');
+    countForm.setAttribute( 'value',' ');
+    
+
+    let minus = document.createElement('input');
+    minus.setAttribute('type', 'button');
+    minus.setAttribute('value', '-');
+
+    let plus = document.createElement('input');
+    plus.setAttribute('type', 'button');
+    plus.setAttribute('value', '+');
+    plus.addEventListener('click', countPlus);
+
+    let countInd = 0;
+    function countPlus(countInd, event) {
+       console.log('ааа на меня нажали!');
+       countInd = countForm.value;
+       console.log(countForm.value);
+       if (countForm.value > 0) {
+        countInd++;
+       }
+       
+    }
+    
+    let maxCount = document.createElement('p');
+    maxCount.innerText= `Всего на складе ${elem.count}`;
+
+    form.append(minus,countForm, plus);
+    booksTable.append(title, author, form, maxCount);
+
+    if(elem.count === 0) {
+        minus.setAttribute('disabled','')
+    }
+
+    if(countForm.value > elem.count) {
+        countForm.setAttribute('value', 'disabled' )
+    }
 }
 
 
